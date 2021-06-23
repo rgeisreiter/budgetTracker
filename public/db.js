bc4a32df627630d01522ae8de90f1510823b1108
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-23 16:09:14 -0600

    created save record

--- public/db.js
+++ public/db.js
@@ -10,5 +10,16 @@ request.onsuccess = ((evt)=> {
     db = evt.target.result;
     if (navigator.onLine) {
         checkDatabase();
-      }
+    }
+})
+
+request.onerror =  ((evt) => {
+    console.log("Error")
+});
+
+const saveRecord = ((record) => {
+    console.log("Started to save record")
+    const transaction = db.transaction(["Pending"], "readwrite");
+    const store = transaction.objectStore("Pending");
+    store.add(record)
 })
