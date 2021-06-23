2eca63f2ee6783f1dbce70df2bb9e24644c7d678
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-23 16:20:51 -0600

    finsihed db

--- public/db.js
+++ public/db.js
@@ -9,7 +9,7 @@ request.onupgradeneeded = ((evt)=> {
 request.onsuccess = ((evt)=> {
     db = evt.target.result;
     if (navigator.onLine) {
-        checkDatabase();
+        checkDB();
     }
 })
 
@@ -23,3 +23,31 @@ const saveRecord = ((record) => {
     const store = transaction.objectStore("Pending");
     store.add(record)
 })
+
+const checkDB = () => {
+    const transaction = db.transaction(["Pending"], "readwrite");
+    const store = transaction.objectStore("Pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess(()=> {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers:{
+                    Accept: "application/json , text/plain, */*", 
+                "Content-Type":"application/json"}
+            })
+            .then(res => {
+                return res.json
+            })
+            .then(()=> {
+                const transaction = db.transaction(["Pending"], "readwrite");
+                const store = transaction.objectStore("Pending");
+                store.clear() //this will delete from index.db
+            })
+        }
+    })
+}
+
+window.addEventListener("online", checkDB)
