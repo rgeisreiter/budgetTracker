7ce446437c4238945b2bb02825b97a7efd663ec5
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-23 15:39:06 -0600

    created fetch event listener

--- public/service-workers.js
+++ public/service-workers.js
@@ -21,3 +21,38 @@ self.addEventListener("install", (evt) => {
         )
     )
 })
+
+
+self.addEventListener("fetch", (evt) => {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(Data_CACHE_Name).then(cache => {
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response
+                
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(()=> {
+            return caches.match(evt.request)
+            .then((res)=> {
+                if(res){
+                    return res
+                }
+                else if (evt.request.headers.get("accept").includes("text/html")){
+                    return caches.match("/")
+                }
+            })
+        })
+    )
+})
