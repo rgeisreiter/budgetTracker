576f6b1fb753f402f3a6c5bc9a99d2edf5d7d041
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-23 15:22:32 -0600

    set up files

---
+++ public/service-workers.js
@@ -0,0 +1,12 @@
+const FileToCache = [
+    "/",
+    "/db",
+    "/index.html",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.json",
+]
+
+const Cache_Name = "static-cache-v1";
+const Data_CACHE_Name = "data-cache-v1"
